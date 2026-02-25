796305fad75ebfdd75b153d5e6489615a3cc2b59
Caleb Reeves <reevescaleb943@gmail.com>
2026-02-25 14:19:13 +0000

    added while loop

--- 3-while-loops.py
+++ 3-while-loops.py
@@ -5,7 +5,18 @@ def main():
     This is used when you don’t know the exact number of times you want 
     the loop to repeat and it depends on another factor.
     """
-    
+    answer= 7
+    is_correct_answer=False
+
+
+    while not is_correct_answer:
+        guess=input("how many continents are there? ")
+        if int(guess) == answer:
+            print("yay,you got it correct:")
+            is_correct_answer = True
+        else:
+            print("nope, that's not right try again")
+
     # guess the correct answer
     # WHILE answer is not correct loop
     # conditional inside while to check the answer
